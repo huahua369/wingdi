2729a8a0d1f114c8e783efb3425c5ebd842e7b50
华仔 <188665600@qq.com>
2020-01-17 17:18:39 +0800

    1

--- src/windowGdi.cpp
+++ src/windowGdi.cpp
@@ -4,7 +4,9 @@
 #include <math.h>
 #include "windowGdi.h"
 #define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image.h"
+#include "stb_image_write.h"
 
 GdiWindow::GdiWindow()
 {
